266a5a91c10faa05b6f5520a2c6e0e7273b92e2f
Yuuichiro KATO <y1row.kato@gmail.com>
2015-06-08 01:01:07 +0900

    users#index, users#create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,91 @@
 class User < ActiveRecord::Base
   self.table_name = 'user'
+
+  include Garage::Representer
+  include Garage::Authorizable
+
+  after_initialize :set_default_value
+
+  has_one :user_shikibetsu
+
+  property :id
+  property :name
+  property :girudo_id
+  property :shotai_id
+  property :shotai_su
+  property :level
+  property :exp
+  property :coin
+  property :mana_p
+  property :crystal_kakin
+  property :crystal_muryo
+  property :premium_p
+  property :seisyo
+  property :fvko
+  property :unit_max
+  property :huriwaike_p
+  property :unit_cost
+  property :skill_cost
+  property :stamina
+  property :stamina_max
+  property :ap
+  property :ap_max
+  property :koei_flg
+  property :final_login_time
+  property :renzoku_login
+  property :total_login
+  property :kouken_p
+  property :battle_su
+  property :battle_sanka_su
+  property :regist_time
+  property :update_time
+  property :account_stop_flg
+
+  property :user_shikibetsu
+
+  def self.build_permissions(perms, other, target)
+    perms.permits! :write
+    perms.permits! :read
+  end
+
+  def build_permissions(perms, other)
+    perms.permits! :write
+    perms.permits! :read
+  end
+
+  private
+
+  def set_default_value
+    now                   = Time.now
+    self.girudo_id        = 0
+    self.shotai_su        = 0
+    self.level            = 1
+    self.exp              = 0
+    self.coin             = 0
+    self.mana_p           = 0
+    self.crystal_kakin    = 0
+    self.crystal_muryo    = 0
+    self.premium_p        = 0
+    self.seisyo           = 0
+    self.fvko             = 0
+    self.unit_max         = 10
+    self.huriwaike_p      = 0
+    self.unit_cost        = 20
+    self.skill_cost       = 10
+    self.stamina          = 30
+    self.stamina_max      = 30
+    self.ap               = 20
+    self.ap_max           = 20
+    self.koei_flg         = 0
+    self.final_login_time = now
+    self.renzoku_login    = 1
+    self.total_login      = 1
+    self.kouken_p         = 0
+    self.battle_su        = 0
+    self.battle_sanka_su  = 0
+    self.regist_time      = now
+    self.update_time      = now
+    self.account_stop_flg = 0
+  end
+
 end
--- app/models/user_shikibetsu.rb
+++ app/models/user_shikibetsu.rb
@@ -1,3 +1,23 @@
 class UserShikibetsu < ActiveRecord::Base
   self.table_name = 'user_shikibetsu'
+
+  include Garage::Representer
+  include Garage::Authorizable
+
+  after_initialize :set_default_value
+
+  belongs_to :user
+
+  property :user_shikibetsu_id
+  property :kishu_info
+  property :update_version
+
+  private
+
+  def set_default_value
+    self.user_shikibetsu_id = SecureRandom.base64(48)
+    self.kishu_info         = 0
+    self.update_version     = 0
+  end
+
 end
