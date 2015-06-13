46f8c83ee5b63672253d97675b6d28758d7d8db2
Yuuichiro KATO <y1row.kato@gmail.com>
2015-06-14 01:53:10 +0900

    garage's configure (token-scope)

--- config/initializers/garage.rb
+++ config/initializers/garage.rb
@@ -1,24 +1,20 @@
+# Garage
 Garage.configure {}
-
-Garage::TokenScope.configure do
-  register :public, desc: 'acessing publicly available data' do
+Garage::TokenScope.configure {
+  register :public do
     access :read, User
-    access :write, User
   end
-end
-
+}
 # DoorKeeper使う
 Garage.configuration.strategy = Garage::Strategy::Doorkeeper
 
 # DoorKeeper側の設定
 Doorkeeper.configure do
-
   # 認証情報(credentials)からリソースオーナーを特定するメソッド
   resource_owner_from_credentials do |routes|
     us = UserShikibetsu.find_by(user_shikibetsu_id: params[:uid])
     us.user if us.present?
   end
-
 end
 
 # アクセストークン取得 (/oauth/token) にてpassword認証を許可
